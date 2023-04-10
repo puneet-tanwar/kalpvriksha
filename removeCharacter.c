ae14e9bb817300a875144fa0ebf0a01ca347a58c
puneet <puneet.tanwar@intimetec.com>
2023-04-10 09:14:57 +0530

    taking input from getchar() and remove char

---
+++ removeCharacter.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+void removeChar(char *str, char c);
+
+int main() {
+    char str[100],len=0,c;
+
+    printf("Enter a string: ");
+    while ((str[len] = getchar()) != '\n') {
+      len++;
+   }
+   printf("Enter the character you want to remove from string: ");
+   c=getchar();
+
+    int i, j;
+
+    for (i = 0, j = 0; str[i] != '\0'; i++) {
+        if (str[i] != c) {
+            str[j++] = str[i];
+        }
+    }
+    str[j] = '\0';
+
+    printf("Result: %s\n", str);
+
+    return 0;
+}
+
+
