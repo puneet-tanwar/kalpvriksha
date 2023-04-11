32fca7370ea12edc70ab9c00ab694485f955ce8a
puneet-tanwar <130287831+puneet-tanwar@users.noreply.github.com>
2023-04-11 11:41:50 +0530

    Update checkIP.c

--- checkIP.c
+++ checkIP.c
@@ -2,35 +2,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int is_valid_ip_address(char *ip_address) {
-    int num = 0, dots = 0;
-    char *ptr = ip_address;
-
-    if (ip_address == NULL) {
-        return 0;
-    }
-
-    while (*ptr != '\0') {
-        if (*ptr == '.') {
-            dots++;
-            if (num < 0 || num > 255 || dots > 3) {
-                return 0;
-            }
-            num = 0;
-        } else if (isdigit(*ptr)) {
-            num = num * 10 + (*ptr - '0');
-        } else {
-            return 0;
-        }
-        ptr++;
-    }
-
-    if (num < 0 || num > 255 || dots != 3) {
-        return 0;
-    }
-
-    return 1;
-}
 
 int main() {
     char str[20];
